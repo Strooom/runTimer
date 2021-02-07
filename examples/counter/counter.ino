849e2322d07b01467613ed0d108c498cbc8d427e
Strooom <pascal.roobrouck@gmail.com>
2021-02-07 12:24:27 +0100

    extra example

---
+++ examples/counter/counter.ino
@@ -0,0 +1,27 @@
+#include <runtimer.h>
+counter counter1(16);              // create a counter to count events, with a count threshold of 16
+intervalTimer timer1(1000);        // also creating an intervalTimer, so we create expire events which we can count
+singleTimer timer2;                // also creating a singleshot timer, this oneshot event will reset the counter...
+
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
+    timer2.start(8000);        // start the timer, with a timerout of 8000 ms
+}
+
+void loop() {
+    if (timer1.expired()) {
+        counter1.increment();
+        Serial.print("'timer1' expired, 'counter1 incremented to ");
+        Serial.println(counter1.value());
+    }
+    if (timer2.expired()) {
+        counter1.reset();
+        Serial.print("'timer2' expired, 'counter1 reset to ");
+        Serial.println(counter1.value());
+        }
+    if (counter1.expired()) {
+            Serial.println("'counter1' expired");
+        counter1.reset();
+        }
+}
