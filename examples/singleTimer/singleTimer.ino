bef6c8c686c18f2b2fd1c1239193245388163cf9
Strooom <pascal.roobrouck@gmail.com>
2022-03-02 10:41:26 +0100

    improve example 1

--- examples/singleTimer/singleTimer.ino
+++ examples/singleTimer/singleTimer.ino
@@ -1,10 +1,14 @@
+#include <Arduino.h>
 #include <runtimer.h>
 singleTimer timer1;        // create a single timer, it does not start yet
 
 void setup() {
-    Serial.begin(115200);
-    Serial.flush();
-    timer1.start(3000);        // start the timer, with a timerout of 3000 ms
+    Serial.begin(115200);          // initialize and cleanup Serial
+    Serial.flush();                //
+    Serial.println("\n\n");        //
+
+    timer1.start(3000);        // start the timer, with a timeout of x ms
+    Serial.println("timer 'timer1' started");
 }
 
 void loop() {
